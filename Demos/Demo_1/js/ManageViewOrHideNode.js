afb701de3bfba4330598e8e6d945df3b710a5af0
sebastianjsam <jsam2601@gmail.com>
2021-05-01 20:46:37 -0500

    Update de metodo de ocultamiento v0.1.0
    Co-Authored-By: sebas2601 <70503400+sebas2601@users.noreply.github.com>

---
+++ Demos/Demo_1/js/ManageViewOrHideNode.js
@@ -0,0 +1,205 @@
+var nodes;
+var edges;
+
+/**
+ * @description Obtiene el nivel de un nodo haciendo la busqueda en los nodos almacenados en el la red
+ * @param id del nodo a obtener el nivel
+ * @returns nivel del nodo de entrada
+ * @author Sebastian Agudelo
+ */
+
+function getLevel(_thisNode) {
+  return nodes.get(_thisNode).level;
+}
+
+/**
+ * @description: todos los nodos secundarios con un nivel superior al padre, recursivamente
+ * @param:network objeto gráfico
+ * @param:_thisNode: el nodo en el que se hizo clic (nodo principal)
+ * @param:_Allnodes: utilizado para contener la matriz de ID de nodo secundario
+ * @returns Todos los nodos hijos de _thisNode
+ */
+function getAllChilds(network, _thisNode, _Allnodes) {
+  var _nodes = network.getConnectedNodes(_thisNode, "to");
+  if (_nodes.length > 0) {
+    for (var i = 0; i < _nodes.length; i++) {
+      getAllChilds(network, _nodes[i], _Allnodes);
+      _Allnodes.push(_nodes[i]);
+    }
+  }
+  return _Allnodes;
+}
+
+/**
+ * @description Obtiene todas las conexiones existentes de un nodo, teniendo
+ * en cuenta el name para saber si es del mismo arbol.
+ * @param:network objeto gráfico
+ * @param:_thisNode: el nodo en el que se hizo clic (nodo principal)
+ * @param:_Allnodes: utilizado para contener la matriz de ID de nodo secundario
+ * @returns nivel del nodo de entrada
+ * @author Sebastian Agudelo
+ */
+function getAllChildsEdges(network, _thisNode, _EdgesN) {
+  var _Edges = network.getConnectedEdges(_thisNode, "to");
+  for (let t = 0; t < _Edges.length; t++) {
+    if (edges.get(_Edges[t]).name == nodes.get(_thisNode).name) {
+      _EdgesN.push(_Edges[t]);
+    }
+  }
+  return _EdgesN;
+}
+
+/**
+ * @description Obtiene todas los Nodos que son hijos de un nodo, pero teniendo en cuenta el name
+ * para saber que pertenece al mismo arbol
+ * @param:nodesChildAll objeto gráfico
+ * @param:nodeRoot: el nodo en el que se hizo clic (nodo principal)
+ * @returns Nodos que son del mismo arbol, y que tienen un nivel superior
+ * @author Sebastian Agudelo
+ */
+function nodeChildLevelTop(nodesChildAll, nodeRoot) {
+  var nodesNew = [];
+  for (let i = 0; i < nodesChildAll.length; i++) {
+    if (
+      getLevel(nodesChildAll[i]) > getLevel(nodeRoot) &&
+      nodes.get(nodesChildAll[i]).name == nodes.get(nodeRoot).name
+    ) {
+      nodesNew.push(nodesChildAll[i]);
+    }
+  }
+  return nodesNew;
+}
+
+/**
+ * @description Obtiene todas las conexiones que existen entre Todos los
+ * nodos Hijos de cualquier Nodo, se elimina la primer conexion pues esta es la que viene
+ * del nodo padre de NodeRoot
+ * @param:NodesChild Todos los nodos que son hijos de un nodo hasta el ultimo nivel
+ * @param:nodeRoot: el nodo en el que se hizo clic (nodo principal)
+ * @returns Todas las conexiones que pertenecen a todos los Nodos hijos
+ * @author Sebastian Agudelo
+ */
+function EdgesAllConectToNodesChild(NodesChild, nodeRoot) {
+  //se obtiene son las lineas de un solo nodo
+  var edgesHijos = getAllChildsEdges(network, nodeRoot, []);
+  delete edgesHijos[0]; //eliminamos la conexion que viene de abajo
+  var EdgesOfAllChild = edgesHijos;
+  for (let x = 0; x < NodesChild.length; x++) {
+    var response = getAllChildsEdges(network, NodesChild[x], []);
+    for (let y = 0; y < response.length; y++) {
+      EdgesOfAllChild.push(response[y]);
+    }
+  }
+  return EdgesOfAllChild;
+}
+
+/**
+ * @description Obtiene un nodo de un nivel mas que el raiz para saber si
+ * este esta visible o no
+ * @param:nodosHijosArray Todos los nodos que son hijos de un nodo hasta el ultimo nivel
+ * @param:NodoRaiz: el nodo en el que se hizo clic (nodo principal)
+ * @returns True si alguno de los nodos hijos es visible False en otro caso
+ * @author Sebastian Agudelo
+ */
+
+function isAnyNodeVisible(childNodesChild, nodeRoot) {
+  for (let r = 0; r < childNodesChild.length; r++) {
+    if (nodes.get(childNodesChild[r]).level == nodes.get(nodeRoot).level + 1) {
+      return nodes.get(childNodesChild[r]).hidden;
+    }
+  }
+  return false;
+}
+
+/**
+ * @description Obtien todos los nodos que son inmediatamente siguientes al nodoRaiz
+ * @param:nodeRoot: el nodo en el que se hizo clic (nodo principal)
+ * @returns lista de nodos inmediatamnete siguientes al nodoRaiz
+ * @author Sebastian Agudelo
+ */
+function nodesNextNodeId(nodeRoot) {
+  var listNodesChilds = getAllChilds(network, nodeRoot, []);
+  var listNodesContiguous = [];
+  for (let i = 0; i < listNodesChilds.length; i++) {
+    if (
+      getLevel(listNodesChilds[i]) == getLevel(nodeRoot) + 1 &&
+      nodes.get(nodeRoot).name == nodes.get(listNodesChilds[i]).name
+    ) {
+      listNodesContiguous.push(listNodesChilds[i]);
+    }
+  }
+  return listNodesContiguous;
+}
+
+/**
+ * @description actualiza todos los nodos  y conexiones de las dos listas entrantes
+ * @param:nodesModify lista de Todas los nodos que se le quieren cambiar el estado de Hidden
+ * @param:edgesModify lista de Todas las conexiones que se le quieren cambiar el estado de Hidden
+ * @param:state true si se quieren ocultar los nodos y conexiones de las dos listas false en otro caso
+ * @author Sebastian Agudelo
+ */
+function modifyListNodesAndEdges(nodesModify, edgesModify, state) {
+  for (var i = 0; i < nodesModify.length; i++) {
+    nodes.update([{ id: nodesModify[i], hidden: state }]);
+  }
+  for (let e = 0; e < edgesModify.length; e++) {
+    edges.update([{ id: edgesModify[e], hidden: state }]);
+  }
+}
+
+/**
+ * @description metodo para hacer visibles a los nodos y conexiones inmediatamente siguientes
+ * @param:NodoRaiz: el nodo en el que se hizo clic (nodo principal)
+ * @author Sebastian Agudelo
+ */
+
+function viewNode(nodeRoot) {
+  //sacamos primero lista de conexiones que interactuan con el evento
+  var edgesModify = getAllChildsEdges(network, nodeRoot, []);
+  var nodesModify = nodesNextNodeId(nodeRoot); //nodos inmendiatamente siguientes al raiz
+  modifyListNodesAndEdges(nodesModify, edgesModify, false);
+}
+
+/**
+ * @description metodo para ocultar a los nodos y conexiones inmediatamente siguientes
+ * @param:nodeRoot: el nodo en el que se hizo clic (nodo principal)
+ * @author Sebastian Agudelo
+ */
+function hideNodes(nodeRoot) {
+  //obtenemos los que son de nivel superior para saber cuantos tiene
+  var nodesModify = nodeChildLevelTop(
+    getAllChilds(network, nodeRoot, []),
+    nodeRoot
+  );
+  var edgesModify = EdgesAllConectToNodesChild(nodesModify, nodeRoot);
+  modifyListNodesAndEdges(nodesModify, edgesModify, true);
+}
+
+/**
+ * @description metodo Principal y verificar si se oculta o se muestra unos nodos
+ * @param:network objeto gráfico
+ * @param:nodeRoot: el nodo en el que se hizo clic (nodo principal)
+ * @author Sebastian Agudelo
+ */
+function manageNodesHideOrView(network, nodeRoot,nodes,edges) {
+    this.Nodesjs=nodes;
+    this.Edgesjs=edges;
+  var nodeChildsAll = nodeChildLevelTop(
+    getAllChilds(network, nodeRoot, []),
+    nodeRoot
+  );
+  if (isAnyNodeVisible(nodeChildsAll, nodeRoot) == false) {
+    console.log("___________Hide____________"+nodes[0]);
+    hideNodes(nodeRoot);
+  } else {
+    console.log("___________view____________"+edges[0]);
+    viewNode(nodeRoot);
+  }
+  network.fit();
+}
+
+/*network.on("click", function (e) {
+  if (e.nodes.length != 0) {
+    manageNodesHideOrView(network, e.nodes[0]);
+  
+});*/
--- Demos/Demo_3/main.js
+++ Demos/Demo_3/main.js
@@ -4,8 +4,9 @@ function readNodes(json) {
     nodes.push({
         id: 0,
         level: 0,
-        label: json.name,
-        hidden: true
+        label: json.name1,
+        hidden: true,
+        name:0
     });
 
     for (const node of json.nodes) {
@@ -27,11 +28,13 @@ function readNodes(json) {
         nodes.push({
             id: node.id,
             level: node.level,
+            name:node.name,
             label: `${node.label}\n${node.percentage}`,
             hidden: (node.level > 1) ? true : false,
             shape: forma1,
             fixed: true,
             color: { background: color }
+            
         });
     }
 
@@ -46,6 +49,7 @@ function readEdges(json) {
             from: edge.from,
             to: edge.to,
             hidden: (edge.from > 0) ? true : false,
+            name:edge.name
         });
     }
 
@@ -57,10 +61,13 @@ var container = document.getElementById('mynetwork');
 var nodes = new vis.DataSet(readNodes(json_ejemplo));
 var edges = new vis.DataSet(readEdges(json_ejemplo));
 
+console.log("nodes: "+readEdges(json_ejemplo));
+
 var data = {
     nodes: nodes,
     edges: edges
 };
+
 
 var options = {
     width: width + 'px',
@@ -115,103 +122,3 @@ network.on("stabilizationIterationsDone", function () {
     network.setOptions({ physics: false });
 });
 
-function ListaIdEdgesEvento(idNodo) {
-    var contador = 0;
-    CapturaEvento = quienesSonLosHijosLineas(idNodo);
-    var nivelInicial = getLevel(idNodo);
-    var listaIdEdgesEvento = [];
-
-    for (var i = 0; i < CapturaEvento.length; i++) {
-        var nivelNodo = getLevel(edges.get(CapturaEvento[i]).to);
-        if (nivelNodo > nivelInicial) {
-            listaIdEdgesEvento[contador] = CapturaEvento[i];
-            contador++;
-        }
-    }
-    return listaIdEdgesEvento;
-}
-
-function getLevel(nodoId) {
-    return nodes.get(nodoId).level;
-}
-
-
-function ListaIdNodosEvento(listaIdEdgesEvento2) {
-    var listaIdNodosEvento = [];
-    for (var i = 0; i < listaIdEdgesEvento2.length; i++) {
-        listaIdNodosEvento[i] = edges.get(listaIdEdgesEvento2[i]).to;
-    }
-    return listaIdNodosEvento;
-}
-
-function quienesSonLosHijosLineas(id) {
-    var temporal = network.getConnectedEdges(id);
-    return temporal;
-}
-
-function OcultarNodos(IdNodo) {
-    var edgesEvento; //los Id de las conexiones que interactuan 
-    var nodesEvento; //Los Id de los nodos que interactuan con el evento
-
-    // Sacamos primero lista de conexiones que interactuan con el evento
-    edgesEvento = ListaIdEdgesEvento(IdNodo);
-    nodesEvento = ListaIdNodosEvento(edgesEvento);
-
-    // console.log("____lineas____", edgesEvento)
-    // console.log("____Nodes____", nodesEvento)
-
-    var inicialLevel = getLevel(nodesEvento);
-    var UltimoLevel = getLevel(nodesEvento[nodesEvento.length - 1]);
-    // console.log("Ultimo level ", UltimoLevel, " nodoID: ", IdNodo, "  ", getLevel(IdNodo));
-
-    // Primero tenemos que actualizar los nodos Hidden=true
-    for (var i = 0; i < nodesEvento.length; i++) {
-
-        console.log("R ", IdNodo, " hijos: ", quienesSonLosHijosLineas(nodesEvento[i]), "true or false:", nodes.get(edges.get(quienesSonLosHijosLineas(nodesEvento[i])[0]).to).hidden)
-        if (nodes.get(edges.get(quienesSonLosHijosLineas(nodesEvento[i])[0]).to).hidden == false) {
-            console.log("++++antes de recursividad ", nodesEvento);
-            OcultarNodos(nodesEvento[i]);
-            console.log("-----despues de recursividad ", nodesEvento);
-        }
-
-        // console.log("llamado:  ", i, nodesEvento[i], " tienen hijos? ", quienesSonLosHijosLineas(nodesEvento[i]));
-        nodes.update([
-            { id: nodesEvento[i], hidden: !nodes.get(nodesEvento[i]).hidden },
-        ])
-    }
-
-    // Como 2 paso tenemos que mostrar las conexiones 
-    for (var i = 0; i < edgesEvento.length; i++) {
-        var conexionAotro = getLevel(edges.get(edgesEvento[i]).to);
-        edges.update([
-            { id: edgesEvento[i], hidden: !edges.get(edgesEvento[i]).hidden },
-        ]);
-    }
-    network.fit();
-}
-
-/*
- * Obtener todos los nodos secundarios
- * red: objeto gráfico
- * _thisNode: el nodo en el que se hizo clic (nodo principal)
- * _Allnodes: utilizado para contener la matriz de ID de nodo secundario
- */
-function getAllChilds(network, _thisNode, _Allnodes) {
-    var _nodes = network.getConnectedNodes(_thisNode, "to");
-    if (_nodes.length > 0) {
-        for (var i = 0; i < _nodes.length; i++) {
-            getAllChilds(network, _nodes[i], _Allnodes);
-            _Allnodes.push(_nodes[i]);
-        }
-    }
-    return _Allnodes;
-};
-
-network.on("click", function (e) {
-    if (e.edges.length > 0) {
-        OcultarNodos(e.nodes[0]);
-    }
-
-    var nodosN = []
-    console.log("Los hijos de ", e.nodes[0], "son", getAllChilds(network, e.nodes[0], nodosN));
-});
