a590f0645a8633b7dd615aa2d61aa902db331f44
sebastianjsam <jsam2601@gmail.com>
2021-08-06 01:02:49 -0500

    example three of asignaturas

---
+++ Demos/Demo_3_peticiones_Http_otroCurso/config.js
@@ -0,0 +1,10 @@
+var color5 = "#9853FD";
+var color4 = "#5F4BE3";
+var color3 = "#5E72FA";
+var color2 = "#4B87E3";
+var color1 = "#53C3FD";
+var forma1 = "ellipse"; // "hexagon", "star", "triangle", "square", "diamond", "database", "ellipse", "circle"
+var forma2 = "square";
+
+var width = 1350;
+var height = 650;
