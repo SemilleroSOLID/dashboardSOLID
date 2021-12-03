fc255dac97fc544b0d1a7325dc2bccfbea791745
Stiven Muñoz Murillo <stivenmm1993@gmail.com>
2021-12-03 00:45:44 -0500

    empaquetando el grafo y reestructurando...

--- Dashboard/modules/grafo/ManageViewOrHideNode.js
+++ Dashboard/modules/grafo/ManageViewOrHideNode.js
@@ -1,15 +1,12 @@
-var Nodesjs = window.nodes;
-var Edgesjs = window.edges;
-
-
 /**
  * @description Obtiene el nivel de un nodo haciendo la busqueda en los nodos almacenados en el la red
  * @param id del nodo a obtener el nivel
+ * @param Nodesjs nodos pertenecientes al modelo
  * @returns nivel del nodo de entrada
  * @author Sebastian Agudelo
  */
 
-function getLevel(_thisNode) {
+function getLevel(_thisNode, Nodesjs) {
   return Nodesjs.get(_thisNode).level;
 }
 
@@ -37,10 +34,12 @@ function getAllChilds(network, _thisNode, _Allnodes) {
  * @param:network objeto gráfico
  * @param:_thisNode: el nodo en el que se hizo clic (nodo principal)
  * @param:_Allnodes: utilizado para contener la matriz de ID de nodo secundario
+ * @param Nodesjs nodos pertenecientes al modelo
+ * @param Edgesjs Enlaces entre nodos del modelo creado posteriormente
  * @returns nivel del nodo de entrada
  * @author Sebastian Agudelo
  */
-function getAllChildsEdges(network, _thisNode, _EdgesN) {
+function getAllChildsEdges(network, _thisNode, _EdgesN, Nodesjs,Edgesjs) {
   var _Edges = network.getConnectedEdges(_thisNode, "to");
   for (let t = 0; t < _Edges.length; t++) {
     if (Edgesjs.get(_Edges[t]).name == Nodesjs.get(_thisNode).name) {
@@ -55,14 +54,15 @@ function getAllChildsEdges(network, _thisNode, _EdgesN) {
  * para saber que pertenece al mismo arbol
  * @param:nodesChildAll objeto gráfico
  * @param:nodeRoot: el nodo en el que se hizo clic (nodo principal)
+ * @param Nodesjs nodos pertenecientes al modelo
  * @returns Nodos que son del mismo arbol, y que tienen un nivel superior
  * @author Sebastian Agudelo
  */
-function nodeChildLevelTop(nodesChildAll, nodeRoot) {
+function nodeChildLevelTop(nodesChildAll, nodeRoot, Nodesjs) {
   var nodesNew = [];
   for (let i = 0; i < nodesChildAll.length; i++) {
     if (
-      getLevel(nodesChildAll[i]) > getLevel(nodeRoot) &&
+      getLevel(nodesChildAll[i], Nodesjs) > getLevel(nodeRoot, Nodesjs) &&
       Nodesjs.get(nodesChildAll[i]).name == Nodesjs.get(nodeRoot).name
     ) {
       nodesNew.push(nodesChildAll[i]);
@@ -77,16 +77,17 @@ function nodeChildLevelTop(nodesChildAll, nodeRoot) {
  * del nodo padre de NodeRoot
  * @param:NodesChild Todos los nodos que son hijos de un nodo hasta el ultimo nivel
  * @param:nodeRoot: el nodo en el que se hizo clic (nodo principal)
+ * @param Nodesjs nodos pertenecientes al modelo
  * @returns Todas las conexiones que pertenecen a todos los Nodos hijos
  * @author Sebastian Agudelo
  */
-function EdgesAllConectToNodesChild(NodesChild, nodeRoot) {
+function EdgesAllConectToNodesChild(NodesChild, nodeRoot, network, Nodesjs) {
   //se obtiene son las lineas de un solo nodo
-  var edgesHijos = getAllChildsEdges(network, nodeRoot, []);
+  var edgesHijos = getAllChildsEdges(network, nodeRoot, [], Nodesjs,Edgesjs);
   delete edgesHijos[0]; //eliminamos la conexion que viene de abajo
   var EdgesOfAllChild = edgesHijos;
   for (let x = 0; x < NodesChild.length; x++) {
-    var response = getAllChildsEdges(network, NodesChild[x], []);
+    var response = getAllChildsEdges(network, NodesChild[x], [], Nodesjs,Edgesjs);
     for (let y = 0; y < response.length; y++) {
       EdgesOfAllChild.push(response[y]);
     }
@@ -99,13 +100,17 @@ function EdgesAllConectToNodesChild(NodesChild, nodeRoot) {
  * este esta visible o no
  * @param:nodosHijosArray Todos los nodos que son hijos de un nodo hasta el ultimo nivel
  * @param:NodoRaiz: el nodo en el que se hizo clic (nodo principal)
+ * @param Nodesjs nodos pertenecientes al modelo
  * @returns True si alguno de los nodos hijos es visible False en otro caso
  * @author Sebastian Agudelo
  */
 
-function isAnyNodeVisible(childNodesChild, nodeRoot) {
+function isAnyNodeVisible(childNodesChild, nodeRoot, Nodesjs) {
   for (let r = 0; r < childNodesChild.length; r++) {
-    if (Nodesjs.get(childNodesChild[r]).level == Nodesjs.get(nodeRoot).level + 1) {
+    if (
+      Nodesjs.get(childNodesChild[r]).level ==
+      Nodesjs.get(nodeRoot).level + 1
+    ) {
       return Nodesjs.get(childNodesChild[r]).hidden;
     }
   }
@@ -115,15 +120,17 @@ function isAnyNodeVisible(childNodesChild, nodeRoot) {
 /**
  * @description Obtien todos los nodos que son inmediatamente siguientes al nodoRaiz
  * @param:nodeRoot: el nodo en el que se hizo clic (nodo principal)
+ * @param Nodesjs nodos pertenecientes al modelo
  * @returns lista de nodos inmediatamnete siguientes al nodoRaiz
  * @author Sebastian Agudelo
  */
-function nodesNextNodeId(nodeRoot) {
+function nodesNextNodeId(nodeRoot, network, Nodesjs) {
   var listNodesChilds = getAllChilds(network, nodeRoot, []);
   var listNodesContiguous = [];
   for (let i = 0; i < listNodesChilds.length; i++) {
     if (
-      getLevel(listNodesChilds[i]) == getLevel(nodeRoot) + 1 &&
+      getLevel(listNodesChilds[i], Nodesjs) ==
+        getLevel(nodeRoot, Nodesjs) + 1 &&
       Nodesjs.get(nodeRoot).name == Nodesjs.get(listNodesChilds[i]).name
     ) {
       listNodesContiguous.push(listNodesChilds[i]);
@@ -137,9 +144,11 @@ function nodesNextNodeId(nodeRoot) {
  * @param:nodesModify lista de Todas los nodos que se le quieren cambiar el estado de Hidden
  * @param:edgesModify lista de Todas las conexiones que se le quieren cambiar el estado de Hidden
  * @param:state true si se quieren ocultar los nodos y conexiones de las dos listas false en otro caso
+ * @param Nodesjs nodos pertenecientes al modelo
+ * @param Edgesjs Enlaces entre nodos del modelo creado posteriormente
  * @author Sebastian Agudelo
  */
-function modifyListNodesAndEdges(nodesModify, edgesModify, state) {
+function modifyListNodesAndEdges(nodesModify, edgesModify, state, Nodesjs,Edgesjs) {
   for (var i = 0; i < nodesModify.length; i++) {
     Nodesjs.update([{ id: nodesModify[i], hidden: state }]);
   }
@@ -151,55 +160,79 @@ function modifyListNodesAndEdges(nodesModify, edgesModify, state) {
 /**
  * @description metodo para hacer visibles a los nodos y conexiones inmediatamente siguientes
  * @param:NodoRaiz: el nodo en el que se hizo clic (nodo principal)
+ * @param Edgesjs Enlaces entre nodos del modelo creado posteriormente
  * @author Sebastian Agudelo
  */
 
-function viewNode(nodeRoot) {
+function viewNode(nodeRoot, network, Nodesjs,Edgesjs) {
   //sacamos primero lista de conexiones que interactuan con el evento
-  var edgesModify = getAllChildsEdges(network, nodeRoot, []);
-  var nodesModify = nodesNextNodeId(nodeRoot); //nodos inmendiatamente siguientes al raiz
-  modifyListNodesAndEdges(nodesModify, edgesModify, false);
+  var edgesModify = getAllChildsEdges(network, nodeRoot, [], Nodesjs,Edgesjs);
+  var nodesModify = nodesNextNodeId(nodeRoot, network, Nodesjs); //nodos inmendiatamente siguientes al raiz
+  modifyListNodesAndEdges(nodesModify, edgesModify, false, Nodesjs,Edgesjs);
 }
 
 /**
  * @description metodo para ocultar a los nodos y conexiones inmediatamente siguientes
- * @param:nodeRoot: el nodo en el que se hizo clic (nodo principal)
+ * @param nodeRoot: el nodo en el que se hizo clic (nodo principal)
+ * @param network interfaz de la vista
+ * @param Nodesjs nodos pertenecientes al modelo
+ * @param Edgesjs Enlaces entre nodos del modelo creado posteriormente
  * @author Sebastian Agudelo
  */
-function hideNodes(nodeRoot) {
+function hideNodes(nodeRoot, network, Nodesjs,Edgesjs) {
   //obtenemos los que son de nivel superior para saber cuantos tiene
   var nodesModify = nodeChildLevelTop(
     getAllChilds(network, nodeRoot, []),
-    nodeRoot
+    nodeRoot,
+    Nodesjs
   );
-  var edgesModify = EdgesAllConectToNodesChild(nodesModify, nodeRoot);
-  modifyListNodesAndEdges(nodesModify, edgesModify, true);
+  var edgesModify = EdgesAllConectToNodesChild(
+    nodesModify,
+    nodeRoot,
+    network,
+    Nodesjs
+  );
+  modifyListNodesAndEdges(nodesModify, edgesModify, true, Nodesjs,Edgesjs);
 }
 
 /**
  * @description metodo Principal y verificar si se oculta o se muestra unos nodos
  * @param:network objeto gráfico
  * @param:nodeRoot: el nodo en el que se hizo clic (nodo principal)
+ * @param Nodesjs nodos pertenecientes al modelo
+ * @param Edgesjs Enlaces entre nodos del modelo creado posteriormente
  * @author Sebastian Agudelo
  */
-function manageNodesHideOrView(network, nodeRoot, nodes, edges) {
+function manageNodesHideOrView(network, nodeRoot, Nodesjs, Edgesjs) {
   var nodeChildsAll = nodeChildLevelTop(
     getAllChilds(network, nodeRoot, []),
-    nodeRoot
+    nodeRoot,
+    Nodesjs
   );
-  if (isAnyNodeVisible(nodeChildsAll, nodeRoot) == false) {
-    console.log("___________Hide____________" + JSON.stringify(nodes));
-    hideNodes(nodeRoot);
+  if (isAnyNodeVisible(nodeChildsAll, nodeRoot, Nodesjs) == false) {
+    console.log("___________Hide____________" + JSON.stringify(Nodesjs));
+    hideNodes(nodeRoot, network, Nodesjs,Edgesjs);
   } else {
-    console.log("___________view____________" + JSON.stringify(edges));
-    viewNode(nodeRoot);
+    console.log("___________view____________" + JSON.stringify(Nodesjs));
+    viewNode(nodeRoot, network, Nodesjs,Edgesjs);
   }
   network.fit();
 }
 
-network.on("click", function (e) {
-  // Si se hace click en un nodo.
-  if (e.nodes.length != 0) {
-    manageNodesHideOrView(network, e.nodes[0], nodes, edges);
-  }
-});
+/**
+ * @description funcion de para iniciar el evento del manejo de ocultamiento ovista de nodos 
+ * @param:networkIn objeto gráfico
+ * @param:nodesIn: Nodos pertenecientes al modelo
+ * @param:edgesIn: Edges pertenecientes al modelo iniciado posteriormente
+ * @author Sebastian Agudelo
+ */
+
+function InitManageViewOrHideNode(networkIn, nodesIn, edgesIn) {
+  this.Edgesjs = edgesIn;
+  networkIn.on("click", function (e) {
+    // Si se hace click en un nodo.
+    if (e.nodes.length != 0) {
+      manageNodesHideOrView(networkIn, e.nodes[0], nodesIn, edgesIn);
+    }
+  });
+}
--- Dashboard/modules/grafo/main.js
+++ Dashboard/modules/grafo/main.js
@@ -1,129 +1,135 @@
 function obtenerColor(porcentaje) {
-    let color = "";
+  let color = "";
 
-    if (porcentaje <= 0) {
-        color = color1;
-    } else if (porcentaje < 30) {
-        color = color2;
-    } else if (porcentaje < 60) {
-        color = color3;
-    } else if (porcentaje < 90) {
-        color = color4;
-    } else if (porcentaje >= 90) {
-        color = color5;
-    }
+  if (porcentaje <= 0) {
+    color = color1;
+  } else if (porcentaje < 30) {
+    color = color2;
+  } else if (porcentaje < 60) {
+    color = color3;
+  } else if (porcentaje < 90) {
+    color = color4;
+  } else if (porcentaje >= 90) {
+    color = color5;
+  }
 
-    return color;
+  return color;
 }
 
 function readNodes(json) {
-    let nodes = [];
+  let nodes = [];
+
+  nodes.push({
+    id: 0,
+    level: 0,
+    label: json.name1,
+    hidden: true,
+    name: 0,
+  });
+
+  for (const node of json.nodes) {
+    let porcentaje = node.percentage.slice(0, -1) * 1;
+    let color = obtenerColor(porcentaje);
 
     nodes.push({
-        id: 0,
-        level: 0,
-        label: json.name1,
-        hidden: true,
-        name: 0
+      id: node.id,
+      level: node.level,
+      name: node.name,
+      label: `${node.label}\n${node.percentage}`,
+      hidden: node.level > 1 ? true : false,
+      shape: forma1,
+      fixed: true,
+      color: { background: color },
     });
+  }
 
-    for (const node of json.nodes) {
-        let porcentaje = node.percentage.slice(0, -1) * 1;
-        let color = obtenerColor(porcentaje);
-
-        nodes.push({
-            id: node.id,
-            level: node.level,
-            name: node.name,
-            label: `${node.label}\n${node.percentage}`,
-            hidden: (node.level > 1) ? true : false,
-            shape: forma1,
-            fixed: true,
-            color: { background: color }
-        });
-    }
-
-    return nodes;
+  return nodes;
 }
 
 function readEdges(json) {
-    let edges = [];
+  let edges = [];
 
-    for (const edge of json.edges) {
-        edges.push({
-            from: edge.from,
-            to: edge.to,
-            hidden: (edge.from > 0) ? true : false,
-            name: edge.name
-        });
-    }
+  for (const edge of json.edges) {
+    edges.push({
+      from: edge.from,
+      to: edge.to,
+      hidden: edge.from > 0 ? true : false,
+      name: edge.name,
+    });
+  }
 
-    return edges;
+  return edges;
 }
 
-var container = document.getElementById('mynetwork');
+function InitMain() {
+  var container = document.getElementById("mynetwork");
 
-var nodes = new vis.DataSet(readNodes(json_ejemplo));
-var edges = new vis.DataSet(readEdges(json_ejemplo));
+  var nodes = new vis.DataSet(readNodes(json_ejemplo));
+  var edges = new vis.DataSet(readEdges(json_ejemplo));
 
-console.log("nodes: " + JSON.stringify(readNodes(json_ejemplo)));
-console.log("edges: " + JSON.stringify(readEdges(json_ejemplo)));
+  console.log("nodes: " + JSON.stringify(readNodes(json_ejemplo)));
+  console.log("edges: " + JSON.stringify(readEdges(json_ejemplo)));
 
-var data = {
+  var data = {
     nodes: nodes,
-    edges: edges
-};
+    edges: edges,
+  };
 
-
-var options = {
-    width: width + 'px',
-    height: height + 'px',
+  var options = {
+    width: "1450px",
+    height: "450px",
     edges: {
-        smooth: { type: "straightCross" },
+      smooth: { type: "straightCross" },
     },
     nodes: {
-        borderWidth: 2, fixed: { x: false, y: false }
+      borderWidth: 2,
+      fixed: { x: false, y: false },
     },
     interaction: {
-        hover: true
+      hover: true,
     },
     physics: {
-        stabilization: true, wind: { x: 0, y: 0 },
+      stabilization: true,
+      wind: { x: 0, y: 0 },
     },
     layout: {
-        improvedLayout: true,
-        hierarchical: {
-            direction: "DU",
-            sortMethod: "hubsize",
-            nodeSpacing: 200,
-            treeSpacing: 200
-        }
+      improvedLayout: true,
+      hierarchical: {
+        direction: "DU",
+        sortMethod: "hubsize",
+        nodeSpacing: 200,
+        treeSpacing: 200,
+      },
     },
     physics: {
-        barnesHut: {
-            centralGravity: 0.2,
-        },
-        maxVelocity: 5,
-        minVelocity: 0.47,
-        solver: "hierarchicalRepulsion"
+      barnesHut: {
+        centralGravity: 0.2,
+      },
+      maxVelocity: 5,
+      minVelocity: 0.47,
+      solver: "hierarchicalRepulsion",
     },
     nodes: {
-        "physics": true
+      physics: true,
     },
     interaction: {
-        navigationButtons: true,
-        keyboard: true,
+      navigationButtons: true,
+      keyboard: true,
     },
-};
+  };
 
-var network = new vis.Network(container, data, options);
+  var network = new vis.Network(container, data, options);
 
-network.moveTo({
+  network.moveTo({
     position: { x: 0, y: 0 },
     offset: { x: -width / 2, y: -height / 2 },
     scale: 1,
-})
+  });
 
-network.on("stabilizationIterationsDone", function () {
+  network.on("stabilizationIterationsDone", function () {
     network.setOptions({ physics: false });
-});
+  });
+
+  InitManageViewOrHideNode(network,nodes,edges);
+
+}
