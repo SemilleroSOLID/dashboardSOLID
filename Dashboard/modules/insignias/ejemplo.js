c118e6887c87ddedf5af4cac9db6edb7420b7eaf
Stiven Muñoz Murillo <stivenmm1993@gmail.com>
2021-12-03 02:31:53 -0500

    modulo de insignias terminado

---
+++ Dashboard/modules/insignias/ejemplo.js
@@ -0,0 +1,20 @@
+function Get() {
+  objJSON = JSON.parse(json);
+  console.log(objJSON);
+
+  pp = objJSON.puntos_puntualidad;
+  document.querySelector("#puntualModal .puntos_obtenidos").innerText = pp;
+  document.querySelector("#puntualModal .puntos_restantes").innerText = 100 - pp;
+
+  pc = objJSON.puntos_conquista;
+  document.querySelector("#conquistadorModal .puntos_obtenidos").innerText = pc;
+  document.querySelector("#conquistadorModal .puntos_restantes").innerText = 100 - pc;
+
+  pb = objJSON.puntos_brillantez;
+  document.querySelector("#brillanteModal .puntos_obtenidos").innerText = pb;
+  document.querySelector("#brillanteModal .puntos_restantes").innerText = 100 - pb;
+
+  pe = objJSON.puntos_estudiante;
+  document.querySelector("#estudiosoModal .puntos_obtenidos").innerText = pe;
+  document.querySelector("#estudiosoModal .puntos_restantes").innerText = 100 - pe;
+}
---
+++ Dashboard/modules/insignias/insignias.js
@@ -0,0 +1,6 @@
+var json = `{
+    "puntos_puntualidad": 60,
+    "puntos_conquista": 40,
+    "puntos_brillantez": 10,
+    "puntos_estudiante": 30
+}`;
